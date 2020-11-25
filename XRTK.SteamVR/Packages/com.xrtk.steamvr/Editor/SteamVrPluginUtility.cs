c9e51785a57f349a52c6bfed2bd1af4cccbb8cf5
Stephen Hodgson <hodgson.designs@gmail.com>
2020-11-25 14:32:06 -0500

    cleaned up the steam vr plugin utility (#17)

--- XRTK.SteamVR/Packages/com.xrtk.steamvr/Editor/SteamVrPluginUtility.cs
+++ XRTK.SteamVR/Packages/com.xrtk.steamvr/Editor/SteamVrPluginUtility.cs
@@ -40,77 +40,93 @@ namespace XRTK.SteamVR.Editor
 
         static SteamVRPluginUtility()
         {
-            if (!Directory.Exists(PluginPath) || EditorPreferences.Get($"Reimport_{nameof(SteamVRPluginUtility)}", false))
+            Debug.Assert(Directory.Exists(NativeRootPath), "Submodule not found! Did you make sure to recursively checkout this branch?");
+
+            if (EditorPreferences.Get($"Reimport_{nameof(SteamVRPluginUtility)}", true))
             {
                 EditorPreferences.Set($"Reimport_{nameof(SteamVRPluginUtility)}", false);
-
-                if (!Directory.Exists(NativeRootPath))
-                {
-                    throw new Exception($"Failed to find {NativeRootPath}");
-                }
-
-                if (Directory.Exists(PluginPath))
-                {
-                    var files = Directory.GetFiles(PluginPath, "*", SearchOption.AllDirectories);
-
-                    foreach (var file in files)
-                    {
-                        File.Delete(file);
-                    }
-
-                    var directories = Directory.GetDirectories(PluginPath, "*", SearchOption.AllDirectories);
-
-                    foreach (var directory in directories)
-                    {
-                        Directory.Delete(directory);
-                    }
-
-                    Directory.Delete(PluginPath);
-                }
-
-                Directory.CreateDirectory($"{PluginPath}/x86");
-                File.Copy($"{NativeRuntime}/x86/{OPEN_VR_API}", $"{PluginPath}/x86/{OPEN_VR_API}");
-
-                Directory.CreateDirectory($"{PluginPath}/x64");
-                File.Copy($"{NativeRuntime}/x64/{OPEN_VR_API}", $"{PluginPath}/x64/{OPEN_VR_API}");
-
-                File.Copy($"{NativeRuntime}/openvr_api.cs", $"{PluginPath}/openvr_api.cs");
-                File.Copy($"{PackageRoot}/LICENSE.md", $"{PluginPath}/LICENSE.md");
-
-                EditorApplication.delayCall += () => AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
-                return;
+                DeleteSupportLibraries();
             }
 
             if (Directory.Exists(PluginPath))
             {
-                var rootPluginPath = $"{SteamVRRootPath}/Runtime/Plugins";
-
-                var x86Path = $"{rootPluginPath}/x86/{OPEN_VR_API}";
-                Debug.Assert(File.Exists(x86Path), $"Library not found at {x86Path}");
-                var x86Importer = AssetImporter.GetAtPath(x86Path) as PluginImporter;
-                Debug.Assert(x86Importer != null, $"Failed to load {x86Path}");
-                x86Importer.ClearSettings();
-                x86Importer.SetCompatibleWithAnyPlatform(false);
-                x86Importer.SetCompatibleWithEditor(true);
-                x86Importer.SetEditorData("CPU", "x86");
-                x86Importer.SetPlatformData(BuildTarget.NoTarget, "CPU", "x86");
-                x86Importer.SetCompatibleWithPlatform(BuildTarget.StandaloneWindows, true);
-                x86Importer.SetPlatformData(BuildTarget.StandaloneWindows, "CPU", "x86");
-                x86Importer.SaveAndReimport();
-
-                var x64Path = $"{rootPluginPath}/x64/{OPEN_VR_API}";
-                Debug.Assert(File.Exists(x64Path), $"Library not found at {x64Path}");
-                var x64Importer = AssetImporter.GetAtPath(x64Path) as PluginImporter;
-                Debug.Assert(x64Importer != null, $"Failed to load {x64Path}");
-                x64Importer.ClearSettings();
-                x64Importer.SetCompatibleWithAnyPlatform(false);
-                x64Importer.SetCompatibleWithEditor(true);
-                x64Importer.SetEditorData("CPU", "x64");
-                x64Importer.SetPlatformData(BuildTarget.NoTarget, "CPU", "x64");
-                x64Importer.SetCompatibleWithPlatform(BuildTarget.StandaloneWindows64, true);
-                x64Importer.SetPlatformData(BuildTarget.StandaloneWindows64, "CPU", "x64");
-                x64Importer.SaveAndReimport();
+                if (EditorPreferences.Get($"SetMeta_{nameof(SteamVRPluginUtility)}", true))
+                {
+                    SetPluginMeta();
+                    EditorPreferences.Set($"SetMeta_{nameof(SteamVRPluginUtility)}", false);
+                }
             }
+            else
+            {
+                CopySupportLibraries();
+                EditorPreferences.Set($"SetMeta_{nameof(SteamVRPluginUtility)}", true);
+                EditorApplication.delayCall += () => AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+            }
+        }
+
+        private static void DeleteSupportLibraries()
+        {
+            if (Directory.Exists(PluginPath))
+            {
+                var files = Directory.GetFiles(PluginPath, "*", SearchOption.AllDirectories);
+
+                foreach (var file in files)
+                {
+                    File.Delete(file);
+                }
+
+                var directories = Directory.GetDirectories(PluginPath, "*", SearchOption.AllDirectories);
+
+                foreach (var directory in directories)
+                {
+                    Directory.Delete(directory);
+                }
+
+                Directory.Delete(PluginPath);
+            }
+        }
+
+        private static void CopySupportLibraries()
+        {
+            Directory.CreateDirectory($"{PluginPath}/x86");
+            File.Copy($"{NativeRuntime}/x86/{OPEN_VR_API}", $"{PluginPath}/x86/{OPEN_VR_API}");
+
+            Directory.CreateDirectory($"{PluginPath}/x64");
+            File.Copy($"{NativeRuntime}/x64/{OPEN_VR_API}", $"{PluginPath}/x64/{OPEN_VR_API}");
+
+            File.Copy($"{NativeRuntime}/openvr_api.cs", $"{PluginPath}/openvr_api.cs");
+            File.Copy($"{PackageRoot}/LICENSE.md", $"{PluginPath}/LICENSE.md");
+        }
+
+        private static void SetPluginMeta()
+        {
+            var rootPluginPath = $"{SteamVRRootPath}/Runtime/Plugins";
+
+            var x86Path = $"{rootPluginPath}/x86/{OPEN_VR_API}";
+            Debug.Assert(File.Exists(x86Path), $"Library not found at {x86Path}");
+            var x86Importer = AssetImporter.GetAtPath(x86Path) as PluginImporter;
+            Debug.Assert(x86Importer != null, $"Failed to load {x86Path}");
+            x86Importer.ClearSettings();
+            x86Importer.SetCompatibleWithAnyPlatform(false);
+            x86Importer.SetCompatibleWithEditor(true);
+            x86Importer.SetEditorData("CPU", "x86");
+            x86Importer.SetPlatformData(BuildTarget.NoTarget, "CPU", "x86");
+            x86Importer.SetCompatibleWithPlatform(BuildTarget.StandaloneWindows, true);
+            x86Importer.SetPlatformData(BuildTarget.StandaloneWindows, "CPU", "x86");
+            x86Importer.SaveAndReimport();
+
+            var x64Path = $"{rootPluginPath}/x64/{OPEN_VR_API}";
+            Debug.Assert(File.Exists(x64Path), $"Library not found at {x64Path}");
+            var x64Importer = AssetImporter.GetAtPath(x64Path) as PluginImporter;
+            Debug.Assert(x64Importer != null, $"Failed to load {x64Path}");
+            x64Importer.ClearSettings();
+            x64Importer.SetCompatibleWithAnyPlatform(false);
+            x64Importer.SetCompatibleWithEditor(true);
+            x64Importer.SetEditorData("CPU", "x64");
+            x64Importer.SetPlatformData(BuildTarget.NoTarget, "CPU", "x64");
+            x64Importer.SetCompatibleWithPlatform(BuildTarget.StandaloneWindows64, true);
+            x64Importer.SetPlatformData(BuildTarget.StandaloneWindows64, "CPU", "x64");
+            x64Importer.SaveAndReimport();
         }
 
         [MenuItem("Mixed Reality Toolkit/Tools/SteamVR/Reimport Plugins", true)]
